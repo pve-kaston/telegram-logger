534a64a8d87f1ece9f3311459f21e6c4486ea051
Illia Savchenko <ilas70587@gmail.com>
2026-02-22 12:50:42 +0100

    Test 4

--- src/telegram_logger/health/healthcheck.py
+++ src/telegram_logger/health/healthcheck.py
@@ -1,5 +1,4 @@
 import json
-import sys
 import logging
 import threading
 from datetime import datetime, timezone
@@ -72,4 +71,4 @@ def setup_healthcheck() -> None:
     server = ThreadingHTTPServer(("0.0.0.0", settings.health_port), _HealthHandler)
     thread = threading.Thread(target=server.serve_forever, daemon=True, name="health-http")
     thread.start()
-    print("Health endpoint on 0.0.0.0:%s%s" % (settings.health_port, settings.health_path), file=sys.stderr, flush=True)
+    logging.getLogger("health").info("Health endpoint on 0.0.0.0:%s%s", settings.health_port, settings.health_path)
